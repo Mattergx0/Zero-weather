9016674796a3e0ff1b7b427587973afcf7ec6859
Mattergx0 <Liamjurgens13@gmail.com>
2025-04-17 22:54:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "9df587fe16e54a6ab6f204233251704"; // Jouw API-sleutel van OpenWeatherMap
+const apiKey = "8a11fa30ad"; // Jouw API-sleutel van OpenWeatherMap
 
 function getWeather() {
     const location = document.getElementById("location").value;

31183e3f274363977cc45852338750cc1a882d48
Mattergx0 <Liamjurgens13@gmail.com>
2025-04-19 12:51:48 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,25 @@
-const CACHE_NAME = 'weer-app-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/manifest.json',
-  '/icon.png',
-  'https://unpkg.com/leaflet/dist/leaflet.css',
-  'https://unpkg.com/leaflet/dist/leaflet.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/webfonts/fa-solid-900.woff2',
+const cacheName = "zero-weather-cache-v1";
+const filesToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/icon.png",
+  "/manifest.json",
 ];
 
-// Installatie van de service worker en cache bestanden
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Cache geopend en bestanden toegevoegd');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Activeren van de service worker
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            console.log('Oude cache verwijderd:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
     })
   );
 });
 
-// Fetch event om de bestanden van cache te halen
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      // Als het bestand in de cache zit, geef dat terug, anders haal het op van het netwerk
-      return cachedResponse || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });

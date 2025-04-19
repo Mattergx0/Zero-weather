8975ea9992eb39272a3e31696bcc22658ccc5859
Mattergx0 <Liamjurgens13@gmail.com>
2025-04-19 13:03:44 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,21 @@
-const CACHE_NAME = 'zero-weather-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/icon.png',
-  'https://unpkg.com/leaflet/dist/leaflet.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-];
-
-// Installeren van de service worker en het cachen van de belangrijke bestanden
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Activeren van de service worker
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
+    event.waitUntil(
+        caches.open('zero-weather-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js',
+                '/icon.png'
+            ]);
         })
-      );
-    })
-  );
+    );
 });
 
-// Haal de bestanden op uit de cache, en indien niet in de cache, ga online
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
 });

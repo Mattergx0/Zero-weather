4767b4ef0d1e825745e8e396a9692252bdcd2443
Mattergx0 <Liamjurgens13@gmail.com>
2025-04-19 14:02:04 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,21 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("zero-weather-v1").then(cache =>
-      cache.addAll([
-        "./",
-        "index.html",
-        "styles.css",
-        "script.js",
-        "icon.png",
-        "manifest.json"
-      ])
-    )
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('zero-weather-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js',
+                '/icon.png'
+            ]);
+        })
+    );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
 });

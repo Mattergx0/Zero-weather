ea8dcd54f504be931c566ed6e72f8939cdbbf471
Mattergx0 <Liamjurgens13@gmail.com>
2025-04-20 22:32:54 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,20 @@
-// service-worker.js
+const CACHE_NAME = 'zeroweather-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/icon.png',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css'
+];
+
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open('zeroweather').then((cache) => 
-        cache.addAll(['/', '/index.html', '/icon.png'])
-    ));
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
 });
